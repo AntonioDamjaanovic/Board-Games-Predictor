deb03afaa9ef271991c4e4287e707fbc11d431b9
AntonioDamjaanovic <damjanovic1109@gmail.com>
2026-01-11 15:42:55 +0100

    Finalize the look of web site

--- BoardGamesPredictor/BoardGamesPredictor/urls.py
+++ BoardGamesPredictor/BoardGamesPredictor/urls.py
@@ -21,6 +21,6 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name = 'home_path'),
+    path('', views.home, name = 'home'),
     path('app/', include('app.urls'))
 ]
